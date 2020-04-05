706ac3ad306528e93439f2a75c6640f765bbace6
Sandali Weththasinghe <37362261+SandaliWeththasinghe@users.noreply.github.com>
2020-04-05 17:55:53 +0530

    http requests

--- index.js
+++ index.js
@@ -2,6 +2,26 @@ const express = require('express');
 
 const app = express();
 
+// Get Requests
+app.get("/se", (req,res) => {
+  return res.json("Get Request");
+});
+
+// Post Requests
+app.post("/se", (req,res) => {
+  return res.json("Post Request");
+});
+
+// Put Requests
+app.put("/se", (req,res) => {
+  return res.json("Put Request");
+});
+
+// Delete Requests
+app.delete("/se", (req,res) => {
+  return res.json("Delete Request");
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
