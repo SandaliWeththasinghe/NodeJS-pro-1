45c5a52a2516beec2e9196782a7704093f8838b7
Sandali Weththasinghe <37362261+SandaliWeththasinghe@users.noreply.github.com>
2020-04-05 20:42:49 +0530

    HTTP requests with parameters

--- index.js
+++ index.js
@@ -7,19 +7,28 @@ app.get("/se", (req,res) => {
   return res.json("Get Request");
 });
 
+// Get with parameter
+app.get("/se/:seNo", (req,res) => {
+  req.params.seNo;
+  return res.json(req.params.seNo);
+});
+
 // Post Requests
-app.post("/se", (req,res) => {
-  return res.json("Post Request");
+app.post("/se/:postPara", (req,res) => {
+  const seNum = req.params.postPara;
+  return res.json(seNum);
 });
 
 // Put Requests
-app.put("/se", (req,res) => {
-  return res.json("Put Request");
+app.put("/se/:putPara", (req,res) => {
+  const seNum = req.params.putPara;
+  return res.json(seNum);
 });
 
 // Delete Requests
-app.delete("/se", (req,res) => {
-  return res.json("Delete Request");
+app.delete("/se/:deletePara", (req,res) => {
+  const seNum = req.params.deletePara;
+  return res.json(seNum);
 });
 
 const PORT = process.env.PORT || 5000;
